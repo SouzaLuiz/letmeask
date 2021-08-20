2375b38c4dcb739acae15721b36ea01c883855df
Luiz Henrique Souza <henryque.souz4@gmail.com>
2021-08-20 19:46:29 -0400

    feat: add disabled styles in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,5 +4,5 @@ import { ButtonHTMLAttributes } from 'react';
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
-  return <button className="font-body bg-primary hover:bg-primary-hover transition w-full p-4 rounded-md text-white" {...props} />;
+  return <button className="font-body bg-primary hover:bg-primary-hover transition w-full p-4 rounded-md text-white disabled:opacity-50" {...props} />;
 }
