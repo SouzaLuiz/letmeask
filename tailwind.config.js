06c99b8d40c40b86ff3d47c76498a2ec09938af5
Luiz Henrique <luizhenrique@going2.com.br>
2022-05-19 21:27:21 -0400

    refactor: ajust tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
